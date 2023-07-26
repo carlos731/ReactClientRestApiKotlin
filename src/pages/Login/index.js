4ec6c388c4587e537b0ad1d845de07bf3bca7782
carlos731 <chtsca2001@gmail.com>
2023-07-25 23:11:16 -0300

    Adicionando projeto front em React ao repositório

---
+++ src/pages/Login/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.css';
+
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png';
+
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="logo" />
+
+                <form>
+                    <h1>Access your Account</h1>
+                    <input placeholder="username" />
+                    <input type="password" placeholder="password" />
+
+                    <button className="button" type="submit" >Login</button>
+                </form>
+            </section>
+
+            <img src={padlock} alt="Login" />
+        </div>
+    )
+}
